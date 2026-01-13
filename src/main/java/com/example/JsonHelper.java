1c2c246f7d1cca912c423091c5732fa6f518cf06
MarioHG470 <kuantika.mhg@gmail.com>
2026-01-13 17:16:31 -0600

    Delete src directory

--- src/main/java/com/example/JsonHelper.java
+++
@@ -1,9 +0,0 @@
-package com.example;
-
-import org.json.JSONObject;
-
-public class JsonHelper {
-    public static JSONObject parseBody(HttpResponse response) {
-        return new JSONObject(response.getBody());
-    }
-}
